95a0f258d033ebf05f8ba52ce5f199be2ef258d1
TyeolRik <kino6147@naver.com>
2021-07-29 07:00:43 +0900

    test

--- polynomial/polynomial_test.go
+++ polynomial/polynomial_test.go
@@ -2,6 +2,8 @@ package polynomial
 
 import (
 	"fmt"
+	"math"
+	"sort"
 	"testing"
 )
 
@@ -26,7 +28,17 @@ func TestLegendrePolynomials(t *testing.T) {
 }
 
 func TestPredict(t *testing.T) {
-	x := -0.8638683
-	pos := LegendrePolynomials(5, true)[5]
-	fmt.Println(pos.Evaluate(x))
+	x := []float64{0.1819336, 0.4222107, 0.7173508, 0.06806584, 0.336313, 0.5157331, 0.6954517, 0.7335832, 0.3385519, 0.2617832}
+	sort.Float64s(x)
+	pos := LegendrePolynomials(5, true)
+	k := 5
+	for i := 0; i < k; i++ {
+		tempSum := 0.0
+		for _, eachX := range x {
+			tempSum += pos[i+1].Evaluate(2*eachX-1) * math.Sqrt2
+		}
+		tempSum = tempSum / math.Sqrt(float64(len(x)))
+		tempSum = tempSum * tempSum
+		fmt.Println(i+1, tempSum)
+	}
 }
